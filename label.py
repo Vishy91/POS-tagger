f092396efdfcfec74c1311c449eb46e7bab01d4e
vaishnavi <vmukundhan@indeed.com>
2019-01-31 22:34:59 -0600

    coomit everything

--- label.py
+++ label.py
@@ -36,7 +36,7 @@ if len(sys.argv) != 3:
 
 (train_file, test_file) = sys.argv[1:3]
 
-print "Learning model to understand the data this has three models. first is a simplified version second is HMM.vaishnavi is testing"
+print "Learning model to understand the data this has three models. first is a simplified version second is HMM.vaishnavi is testing. hellooooo this is crap."
 solver = Solver()
 train_data = read_data(train_file)
 solver.train(train_data)
